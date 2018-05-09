efac16a3f5f4144eeca8deeafa89f92b9214dfe2
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-08 22:23:28 -0300

    allow cors

--- src/config/server.js
+++ src/config/server.js
@@ -2,10 +2,12 @@ const port = 3003;
 const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
+const allowCors = require('./cors');
 
 server
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
+  .use(allowCors)
 
 server.listen(port, function() {
   console.log(`Backend is running on port ${port}`);
